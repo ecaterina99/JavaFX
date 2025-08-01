7788c2159f242db6bc0397c2b23954c746ceeff9
ecaterina99 <tricolicikatea1999@gmail.com>
2025-08-01 11:15:55 +0300

    JavaFX + FXML

---
+++ FXMLTest/src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        URL fxmlURL = getClass().getClassLoader().getResource("add_person.fxml");
+        GridPane root = FXMLLoader.load(fxmlURL);
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
